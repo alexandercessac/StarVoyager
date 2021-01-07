67212ced07a7f4d663922cba9a0eac572f18df5b
AlexanderCessac <SmilingLion@protonmail.com>
2021-01-07 10:23:12 -0800

    move splash function to ui.c

--- src/ui.h
+++ src/ui.h
@@ -7,6 +7,8 @@ void fin();
 
 int get_bg();
 
+void DoSplashIntro(int YMAX, int XMAX, int YMID, int XMID);
+
 void render(int ymax,int xmax,int ydiff,int xdiff, int map[1000][1000]);
 void renderDiff(int ymax,int xmax,int ydiff,int xdiff, int map[1000][1000], char direction);
 
